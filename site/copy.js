b724d4ebc067ba3a70e99b356a47b46f6b7c2fab
Hirotoshi Uchida <uho02741358@gmail.com>
2026-01-03 16:28:12 +0900

    Delete site directory

--- site/copy.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("copy").addEventListener("click", async () => {
-  const html = document.getElementById("content").innerHTML;
-  await navigator.clipboard.writeText(html);
-  alert("Rendered HTML copied to clipboard");
-});
